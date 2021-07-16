a3829bd6becd217fa8e598f837b8527f6b912190
Kiranmayi-s <83873537+Kiranmayi-s@users.noreply.github.com>
2021-07-16 11:35:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("hell").innerText = 10;
+    setTimeout(() => {
+        document.getElementById("hell").innerText = 9;
+        setTimeout(() => {
+            document.getElementById("hell").innerText = 8;
+            setTimeout(() => {
+                document.getElementById("hell").innerText = 7;
+                setTimeout(() => {
+                    document.getElementById("hell").innerText = 6;
+                    setTimeout(() => {
+                        document.getElementById("hell").innerText = 5;
+                        setTimeout(() => {
+                            document.getElementById("hell").innerText = 4;
+                            setTimeout(() => {
+                                document.getElementById("hell").innerText = 3;
+                                setTimeout(() => {
+                                    document.getElementById("hell").innerText = 2;
+                                    setTimeout(() => {
+                                        document.getElementById("hell").innerText = 1;
+                                        setTimeout(() => {
+                                            document.getElementById("hell").innerText = "Happy Independence Day";
+                                            
+                                        }, 1000); 
+                                    }, 1000); 
+                                }, 1000); 
+                            }, 1000); 
+                        }, 1000); 
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000); 
+}, 1000);
